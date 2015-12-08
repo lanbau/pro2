3fe109dba9c84c5a2126c8390a3f1df9f9c08435
lanbau <irvinln@gmail.com>
2015-12-08 18:16:43 +0800

    Resolved some standard JS formatting

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var lock = new Auth0Lock(
         // These properties are set in auth0-variables.js
         AUTH0_CLIENT_ID,
         AUTH0_DOMAIN
     );
 
-    var userProfile;
+    var userProfile
 
-    document.getElementById('btn-login').addEventListener('click', function() {
-      lock.show(function(err, profile, token) {
+    document.getElementById('btn-login').addEventListener('click', function () {
+      lock.show(function (err, profile, token) {
         if (err) {
           // Error callback
           console.error("Something went wrong: ", err);
@@ -34,74 +34,32 @@ $(document).ready(function() {
     document.getElementById('btn-api').addEventListener('click', function() {
         console.log(userProfile.identities[0].access_token)
         var at = userProfile.identities[0].access_token
-        FB.getLoginStatus(function(response) {
+        FB.getLoginStatus(function (response) {
+          console.log(response)
           if (response.status === 'connected') {
-            // var obj
-            // FB.api('/me', {fields: 'adaccounts'}, function(response) {
-            //   console.log(response);
-            //   obj = response;
-            //   console.log(obj.adaccounts.data[0].account_id)
-            //   var adaccount = obj.adaccounts.data[0].account_id
-            //   var URL = "https://graph.facebook.com/v2.5/act_" + x + "/adsets?access_token=" + at
-
-            //   function loadXMLDoc(URL) {
-            //     var xmlhttp;
-
-            //     if (window.XMLHttpRequest) {
-            //         // code for IE7+, Firefox, Chrome, Opera, Safari
-            //         xmlhttp = new XMLHttpRequest();
-            //     } else {
-            //         // code for IE6, IE5
-            //         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            //     }
-
-            //     xmlhttp.onreadystatechange = function() {
-            //         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-            //            if(xmlhttp.status == 200){
-            //                document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-            //            }
-            //            else if(xmlhttp.status == 400) {
-            //               alert('There was an error 400')
-            //            }
-            //            else {
-            //                alert('something else other than 200 was returned')
-            //            }
-            //         }
-            //     }
-
-            //         xmlhttp.open("GET", URL , true);
-            //         xmlhttp.send();
-            //     }
-
-            //     loadXMLDoc(URL)
-            // }) '/act_10151519506253453/campaigns'
-          var adnum
-          var adstring
-          FB.api('/me', {fields: 'adaccounts'}, function(response) {
-            console.log(response)
-            adnum = response.adaccounts.data[0].account_id
-            adstring = '/act_'+adnum+'/campaigns'
-            console.log(adstring)
-            FB.api(
-              adstring,
-              'GET',
-              {"fields":"id,name,start_time,effective_status"},
-              function(response) {
-                  // Insert your code here
-                  console.log(response)
-              }
-            );
-          })
-
-
+            var adnum
+            var adstring
+            FB.api('/me', {fields: 'adaccounts'}, function (response) {
+              console.log(response)
+              adnum = response.adaccounts.data[0].account_id
+              adstring = '/act_'+ adnum +'/campaigns'
+              console.log(adstring)
+              FB.api(
+                adstring,
+                'GET',
+                {"fields":"id,name,start_time,effective_status"},
+                function(response) {
+                    // Insert your code here
+                    console.log(response)
+                }
+              )
+            })
           } else if (response.status === 'not_authorized') {
-            // the user is logged in to Facebook, 
+            // the user is logged in to Facebook,
             // but has not authenticated your app
           } else {
             // the user isn't logged in to Facebook.
           }
-         });
+         })
     })
-
-
-});
+})
