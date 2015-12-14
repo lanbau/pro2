aa9a13878e2a700726bba67309e11a07a00a2b29
lanbau <irvinln@gmail.com>
2015-12-14 09:22:55 +0800

    add in required dependencies

--- public/javascripts/auth0-variables.js
+++ public/javascripts/auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='wzfrlWY5jw18UYih9ZKwJWfunkp5uOf0'; 
-var AUTH0_DOMAIN='lanbau.auth0.com'; 
-var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_CLIENT_ID = 'wzfrlWY5jw18UYih9ZKwJWfunkp5uOf0'
+var AUTH0_DOMAIN = 'lanbau.auth0.com'
+var AUTH0_CALLBACK_URL = location.href
--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,109 +1,100 @@
-$(document).ready(function () {
-    var lock = new Auth0Lock(
-        // These properties are set in auth0-variables.js
-        AUTH0_CLIENT_ID,
-        AUTH0_DOMAIN
-    )
-    var userProfile
-    document.getElementById('btn-login').addEventListener('click', function () {
-      lock.show(function (err, profile, token) {
-        if (err) {
-          // Error callback
-          console.error("Something went wrong: ", err)
-          alert("Something went wrong, check the Console errors")
-        } else {
-          // Success calback
-          // Save the JWT token.
-          localStorage.setItem('userToken', token)
-          // Save the profile
-          userProfile = profile
-          document.getElementById('login-box').style.display = 'none'
-          document.getElementById('logged-in-box').style.display = 'inline'
-          document.getElementById('nick').textContent = profile.nickname
-          console.log(token)
-        }
-      })
+/* global Auth0Lock, AUTH0_CLIENT_ID, AUTH0_DOMAIN, FB */
+document.addEventListener('DOMContentLoaded', function () {
+  var lock = new Auth0Lock(
+      // These properties are set in auth0-variables.js
+      AUTH0_CLIENT_ID,
+      AUTH0_DOMAIN
+  )
+  document.getElementById('btn-login').addEventListener('click', function () {
+    lock.show(function (err, profile, token) {
+      if (err) {
+        // Error callback
+        console.error('Something went wrong: ', err)
+        window.alert('Something went wrong, check the Console errors')
+      } else {
+        // Successful callback
+        // Save the JWT token.
+        window.localStorage.setItem('userToken', token)
+        // Save the profile
+        // userProfile = profile
+        document.getElementById('login-box').style.display = 'none'
+        document.getElementById('logged-in-box').style.display = 'inline'
+        document.getElementById('nick').textContent = profile.nickname
+      }
     })
-    document.getElementById('btn-api').addEventListener('click', function () {
-        document.getElementById('top').style.display = 'none'
-        document.getElementById('logged-in-box').style.display = 'none'
-        document.getElementById('wrapper').style.display = 'inline-block'
-        console.log(userProfile.identities[0].access_token)
-        var at = userProfile.identities[0].access_token
-        FB.getLoginStatus(function (response) {
-          console.log(response)
-          if (response.status === 'connected') {
-            var adnum
-            var adstring
-            FB.api('/me', {fields: 'adaccounts'}, function (response) {
-              console.log(response)
-              adnum = response.adaccounts.data[0].account_id
-              adstring = '/act_'+ adnum +'/campaigns'
-              console.log(adstring)
-              // Get campaign
-              FB.api(
-                adstring,
-                'GET',
-                {"fields":"id,name,start_time,stop_time,effective_status,created_time,adlabels,objective,spend_cap"},
-                function(response) {
-                    var arr = response.data
-                    console.log(response)
-                    console.log(arr)
-                    arr.forEach(function(obj){
-                      console.log(obj)
-                      var panel = document.createElement('div')
-                      panel.className = 'panel panel-default'
-                      var panelhead = document.createElement('div')
-                      panelhead.className = 'panel-heading'
-                      var newtable = document.createElement('table')
-                      newtable.className = 'table'
-                      // var tabl = document.getElementById('tabl')
-                      var thead = document.createElement('thead')
-                      var tbody = document.createElement('tbody')
+  })
+  // Go Dashboard Button
+  document.getElementById('btn-api').addEventListener('click', function () {
+    document.getElementById('top').style.display = 'none'
+    document.getElementById('logged-in-box').style.display = 'none'
+    document.getElementById('wrapper').style.display = 'inline-block'
+    FB.getLoginStatus(function (response) {
+      if (response.status === 'connected') {
+        var adnum
+        var adstring
+        FB.api('/me', {fields: 'adaccounts'}, function (response) {
+          adnum = response.adaccounts.data[0].account_id
+          adstring = '/act_' + adnum + '/campaigns'
+          // Get campaign
+          FB.api(
+            adstring,
+            'GET',
+            {'fields': 'id,name,start_time,stop_time,effective_status,created_time,adlabels,objective,spend_cap'},
+            function (response) {
+              var arr = response.data
+              arr.forEach(function (obj) {
+                var panel = document.createElement('div')
+                panel.className = 'panel panel-default'
+                var panelhead = document.createElement('div')
+                panelhead.className = 'panel-heading'
+                var newtable = document.createElement('table')
+                newtable.className = 'table'
+                // var tabl = document.getElementById('tabl')
+                var thead = document.createElement('thead')
+                var tbody = document.createElement('tbody')
 
-                      var tr = document.createElement('tr')
-                      // Table Cell
-                      var leftRow = document.createElement('td')
-                      leftRow.innerHTML = "Name"
-                      tr.appendChild(leftRow)
-                      var rightRow = document.createElement('td')
-                      rightRow.innerHTML = "Value"
-                      tr.appendChild(rightRow)
-                      thead.appendChild(tr)
-                      newtable.appendChild(thead)
-                      // Add the table rows
-                      // use for of 
-                      // Object.keys(obj).forEach(...)
-                      // for (const [key, val] of Object.entries(obj)) { ... }
-                      for (var name in obj) {
-                          var value = obj[name]
-                          var tr = document.createElement('tr')
-                          var leftRow = document.createElement('td')
-                          leftRow.innerHTML = name
-                          tr.appendChild(leftRow)
-                          var rightRow = document.createElement('td')
-                          rightRow.innerHTML = value
-                          tr.appendChild(rightRow)
-                          tbody.appendChild(tr)
-                      }
-                      newtable.appendChild(tbody)
-                      panel.appendChild(panelhead)
-                      panel.appendChild(newtable)
-                      var mytable = document.getElementById('tabl')
-                      mytable.appendChild(panel)
-                    })
-                    
+                var tr = document.createElement('tr')
+                // Table Cell
+                var leftRow = document.createElement('td')
+                leftRow.innerHTML = 'Name'
+                tr.appendChild(leftRow)
+                var rightRow = document.createElement('td')
+                rightRow.innerHTML = 'Value'
+                tr.appendChild(rightRow)
+                thead.appendChild(tr)
+                newtable.appendChild(thead)
+                // Add the table rows
+                // use for of
+                // Object.keys(obj).forEach(...)
+                // for (const [key, val] of Object.entries(obj)) { ... }
+                for (var name in obj) {
+                  var value = obj[name]
+                  var tr = document.createElement('tr')
+                  var leftRow = document.createElement('td')
+                  leftRow.innerHTML = name
+                  tr.appendChild(leftRow)
+                  var rightRow = document.createElement('td')
+                  rightRow.innerHTML = value
+                  tr.appendChild(rightRow)
+                  tbody.appendChild(tr)
                 }
-              )
-            })
-          } else if (response.status === 'not_authorized') {
-            // the user is logged in to Facebook,
-            // but has not authenticated your app
-          } else {
-            // the user isn't logged in to Facebook.
-          }
-         })
+                newtable.appendChild(tbody)
+                panel.appendChild(panelhead)
+                panel.appendChild(newtable)
+                var mytable = document.getElementById('tabl')
+                mytable.appendChild(panel)
+              })
+            }
+          )
+        })
+      } else if (response.status === 'not_authorized') {
+        // the user is logged in to Facebook,
+        // but has not authenticated your app
+      } else {
+        // the user isn't logged in to Facebook.
+      }
     })
+  })
   // Aussie Map
   document.getElementById('getcon').addEventListener('click', function () {
       document.getElementById('aussie').style.display = 'block'
@@ -230,7 +221,7 @@ $(document).ready(function () {
                       thead.appendChild(tr)
                       newtable.appendChild(thead)
                       // Add the table rows
-                      // use for of 
+                      // use for of
                       // Object.keys(obj).forEach(...)
                       // for (const [key, val] of Object.entries(obj)) { ... }
                       for (var name in obj) {
@@ -251,9 +242,11 @@ $(document).ready(function () {
                       mytable.appendChild(panel)
                     })
         }
-      );
+      )
   })
-
+  document.getElementById('btn-profile').addEventListener('click', function () {
+    document.location.href='/profile'
+  })
   window.onbeforeunload = function (e) {
     e = e || window.event;
 
